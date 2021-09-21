6b07c473967bb875c93a6d21a0f85c0895b09225
julius.suominen <julius.suominen@gmail.com>
2021-09-21 12:45:13 +0300

    add executeTransaction

--- contracts/MultiSignatureWallet.sol
+++ contracts/MultiSignatureWallet.sol
@@ -6,7 +6,8 @@ contract MultiSignatureWallet {
     // indexed makes the event easily searchable
     event Submission(uint indexed transactionId);
     event Confirmation(address indexed sender, uint indexed transactionId);
-
+    event Execution(uint indexed transactionId);
+    event ExecutionFailure(uint indexed transactionId);
 
     address[] public owners;
     uint public required;
@@ -95,7 +96,23 @@ contract MultiSignatureWallet {
 
     /// @dev Allows anyone to execute a confirmed transaction.
     /// @param transactionId Transaction ID.
-    function executeTransaction(uint transactionId) public {}
+    function executeTransaction(uint transactionId)
+        public
+    {
+        require(transactions[transactionId].executed == false);
+        if (isConfirmed(transactionId)) {
+            // using the "storage" keyword makes "t" a pointer to storage 
+            Transaction storage t = transactions[transactionId];
+            t.executed = true;
+            (bool success, bytes memory returnedData) = t.destination.call.value(t.value)(t.data);
+            if (success) {
+                emit Execution(transactionId);
+            } else {
+                emit ExecutionFailure(transactionId);
+                t.executed = false;
+            }
+        }
+    }
 
 		/*
 		 * (Possible) Helper Functions
@@ -103,7 +120,21 @@ contract MultiSignatureWallet {
     /// @dev Returns the confirmation status of a transaction.
     /// @param transactionId Transaction ID.
     /// @return Confirmation status.
-    function isConfirmed(uint transactionId) internal view returns (bool) {}
+    function isConfirmed(uint transactionId)
+        internal
+        view
+        returns (bool)
+    {
+        uint count = 0;
+        for (uint i = 0; i < owners.length; i++) {
+            if (confirmations[transactionId][owners[i]]) {
+                count += 1;
+            }
+            if (count == required) {
+                return true;
+            }
+        }
+    }
 
     /// @dev Adds a new transaction to the transaction mapping, if transaction does not exist yet.
     /// @param destination Transaction target address.
